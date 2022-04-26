17a4abf3ba37870c7f111e7e06288cadf2c0bce4
im3dabasia <eshaandabasiya@gmail.com>
2022-04-26 21:18:18 +0530

    Added User values for our movie database

---
+++ userInsertValues.sql
@@ -0,0 +1,28 @@
+
+Insert into webUser values('AU100', 'Gill', 'Sinha', 'amitsinhT04@gmail.com', 35, '7750912345');
+Insert into webUser values('AU101', 'Mary', 'Seth', 'seth.raghav987@gmail.com', 12, '7845279834');
+Insert into webUser values('AU102', 'anjali', 'Gupta', 'anjali23g@gmail.com', 55, '8849273345');
+Insert into webUser values('AU103', 'maria', 'Mathew', 'jmathew.123@gmail.com', 22, '9846273634');
+Insert into webUser values('AU104', 'kolp', 'Sunil', 'sudha_sunil07@gmail.com', 12, '7732312345');
+Insert into webUser values('AU105', 'manas', 'Kumar', 'kumarajayv56@gmail.com', 43, '9078985643');
+Insert into webUser values('AU106', 'aditya', 'Joshi', 'jvikram.89@gmail.com', 21, '4567345678');
+Insert into webUser values('AU107', 'janiu', 'Agarwal', 'jainu.agarwal87@gmail.com', 12, '9345687654');
+Insert into webUser values('AU108', 'Maitri', 'Shah', 'maitrishahj1@gmail.com', 32, '7180867407');
+Insert into webUser values('AU109', 'keua', 'Shastri', 'bhavyashastri@gmail.com', 25, '8567409098');
+Insert into webUser values('AU110', 'Preeti', 'Jain', 'preeti.jain@gmail.com', 18, '7845279834');
+Insert into webUser values('AU111', 'Shreya', 'Rathod','rathod_shreya@gmail.com',38,'9800215673');
+Insert into webUser values('AU112', 'isha', 'Rajesh','kasturi@gmail.com',21,'9055932134');
+Insert into webUser values('AU113', 'Keerti', 'Srivatsan','keertisri@gmail.com',21,'7707378654');
+Insert into webUser values('AU114', 'kall', 'Raghav','kailragh@gmail.com',23,'9591990037');
+Insert into webUser values('AU115', 'Shrushti', 'Singh','shrushtikaul@gmail.com',12,'9922345016');
+Insert into webUser values('AU116', 'Mona', 'Mohammed','maonamaon@gmail.com',23,'9591670037');
+Insert into webUser values('AU117', 'virat', 'Sharma','viratsha@gmail.com',40,'9902432497');
+Insert into webUser values('AU118', 'Yohanan', 'Veerappan','yohanv@gmail.com',47,'8581654798');
+Insert into webUser values('AU119', 'Yuxwvera', 'Raghavendran','yuzwiragh@gmail.com',12,'9594390037');
+Insert into webUser values('AU120', 'Janice', 'Fernandes','janicefern@gmail.com',55,'9325880785');
+Insert into webUser values('AU121', 'arjun', 'Suresh','arjun1995@gmail.com',66,'9325880785');
+Insert into webUser values('AU122', 'Utham', 'Aiyappa','utham10@gmail.com',24,'1235789456');
+Insert into webUser values('AU123', 'Pranav', 'Gowda','pranavgowda06@gmail.com',47,'7191088921');
+Insert into webUser values('AU124', 'jay', 'Prabhu','jaypr@gmail.com',30,'9098107054');
+Insert into webUser values('AU125', 'Freya', 'Bhandarkar','freyabhand@gmail.com',18,'9895287453');
+
