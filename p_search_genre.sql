84fd7046b31713f253af7a4ace7d7891d849e6a6
im3dabasia <eshaandabasiya@gmail.com>
2022-04-30 01:57:05 +0530

    Added 2nd procedure

---
+++ p_search_genre.sql
@@ -0,0 +1,11 @@
+
+create or replace procedure search_genre(gen varchar) as
+
+BEGIN
+  FOR rec IN (SELECT Movie.name 
+                FROM Movie
+                WHERE Movie.genre = gen)
+  LOOP
+    DBMS_OUTPUT.PUT_LINE ('Movie Name: ' || rec.name);
+  END LOOP;
+END;
